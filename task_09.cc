b0045ddcf55f7831c5602861fd87f43ee6d2ef4a
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-06-30 20:26:17 +0300

    Поиск двух одинарных чисел в массиве.

--- task_09.cc
+++ task_09.cc
@@ -9,39 +9,42 @@
 // При этом, решить задачу надо за линейное время и константную сложность
 // по памяти.
 //
+//------------------------------------------------------------------------------
 #include <cstdlib>
-#include <cstdint>
 #include <iostream>
 #include <array>
 #include <utility>
 //------------------------------------------------------------------------------
 #define LEN_ARRAY   6
 //------------------------------------------------------------------------------
-auto two_single_numbers(const std::array<std::int32_t,LEN_ARRAY>& a)
+using namespace std;
+//------------------------------------------------------------------------------
+auto two_single_numbers(const array<int,LEN_ARRAY>& a)
 {
-   std::int32_t common_xor {0};
-   for (const auto& it: a) {common_xor^=it;}
+   int common_xor {0};
+   for (const auto& it : a) {common_xor^=it;}
 
-   std::int32_t shift {-1};
+   int shift {-1};
    while (!((common_xor >> ++shift)&1)) {}
 
-   std::int32_t mask {1<<shift};
-   std::int32_t group_1_xor {0};
-   std::int32_t group_2_xor {0};
-   for (const auto& it: a) {
-      if (it&mask) {group_1_xor^=it;}
+   int mask {1<<shift};
+   int group_1_xor {0};
+   int group_2_xor {0};
+   for (const auto& it : a) {
+      if (it & mask) {group_1_xor^=it;}
       else {group_2_xor^=it;}
    }
-   return (std::pair<std::int32_t,std::int32_t>(group_1_xor,group_2_xor));
+   return pair<int,int>(group_1_xor,group_2_xor);
 }
 ////////////////////////////////////////////////////////////////////////////////
-std::int32_t main(std::int32_t argc, char** argv)
+int main()
 {
-   std::array<std::int32_t,LEN_ARRAY> a = {1,2,3,4,2,1};
+   array<int,LEN_ARRAY> a = {1,2,3,4,2,1};
 
-   std::pair<std::int32_t,std::int32_t> n;
+   pair<int,int> n;
    n = two_single_numbers(a);
-   std::cout << n.first << ' ' << n.second << '\n';
+   cout << n.first << ' ' << n.second << '\n';
 
-   std::exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 }
+
